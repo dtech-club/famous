26ef50b646b504e5e7eadca7bc4e1fc19c83f96d
dcistudent <dci-student@dcistudent>
2023-05-22 15:34:36 +0200

    tried to do search bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,33 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home.jsx';
-import Navbar from './components/Navbar.jsx';
-import Artists from './components/Artists.jsx';
-import Albums from './components/Albums.jsx';
-import Login from './components/Login.jsx';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Artists from "./components/Artists.jsx";
+import Albums from "./components/Albums.jsx";
+import Login from "./components/Login.jsx";
 //import Signup from './components/Signup.jsx';
-import CreateArtist from './components/addingArtistByAdmin/CreateArtist.jsx';
-import CreateAlbum from './components/addingArtistByAdmin/CreateAlbum.jsx';
-import CreateTrack from './components/addingArtistByAdmin/CreateTrack.jsx';
+import CreateArtist from "./components/addingArtistByAdmin/CreateArtist.jsx";
+import CreateAlbum from "./components/addingArtistByAdmin/CreateAlbum.jsx";
+import CreateTrack from "./components/addingArtistByAdmin/CreateTrack.jsx";
+import SearchBar from "./components/SearchBar.jsx";
 
 function App() {
-    return (
-        <HashRouter>
-            <div className="App">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/artists" element={<Artists />} />
-                    <Route path="/albums" element={<Albums />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/createartist" element={<CreateArtist />} />
-                    <Route path="/createalbum" element={<CreateAlbum />} />
-                    <Route path="/createtrack" element={<CreateTrack />} />
-                </Routes>
-            </div>
-        </HashRouter>
-    );
+  return (
+    <HashRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/search" element={<SearchBar />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/artists" element={<Artists />} />
+          <Route path="/albums" element={<Albums />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/createartist" element={<CreateArtist />} />
+          <Route path="/createalbum" element={<CreateAlbum />} />
+          <Route path="/createtrack" element={<CreateTrack />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
 
 export default App;
