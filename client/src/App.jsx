097d933e906bcf6cbe6e4b8dbe586f59e9b06183
maurofilippifarmar <mauro.filippifarmar@gmail.com>
2023-05-24 11:15:58 +0200

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,31 +5,7 @@ import Artists from "./components/Artists.jsx";
 import Albums from "./components/Albums.jsx";
 import Login from "./components/Login.jsx";
 //import Signup from './components/Signup.jsx';
-<<<<<<< HEAD
-import CreateArtist from "./components/addingArtistByAdmin/CreateArtist.jsx";
-import CreateAlbum from "./components/addingArtistByAdmin/CreateAlbum.jsx";
-import CreateTrack from "./components/addingArtistByAdmin/CreateTrack.jsx";
-import SearchBar from "./components/SearchBar.jsx";
 
-function App() {
-  return (
-    <HashRouter>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/search" element={<SearchBar />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/artists" element={<Artists />} />
-          <Route path="/albums" element={<Albums />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/createartist" element={<CreateArtist />} />
-          <Route path="/createalbum" element={<CreateAlbum />} />
-          <Route path="/createtrack" element={<CreateTrack />} />
-        </Routes>
-      </div>
-    </HashRouter>
-  );
-=======
 import CreateArtist from './components/addingArtistByAdmin/CreateArtist.jsx';
 import CreateAlbum from './components/addingArtistByAdmin/CreateAlbum.jsx';
 import CreateTrack from './components/addingArtistByAdmin/CreateTrack.jsx';
@@ -53,7 +29,7 @@ function App() {
             </div>
         </HashRouter>
     );
->>>>>>> e0a24555de81da62af542b9f5f5284bb4cea629f
+
 }
 
 export default App;
