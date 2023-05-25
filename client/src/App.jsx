f7ba01806ce6f7b38b458477b9360a9290e33d56
maurofilippifarmar <mauro.filippifarmar@gmail.com>
2023-05-25 10:35:45 +0200

    fixed structure folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home.jsx";
-import Navbar from "./components/Navbar.jsx";
-import Artists from "./components/Artists.jsx";
-import Albums from "./components/Albums.jsx";
-import Login from "./components/Login.jsx";
-//import Signup from './components/Signup.jsx';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
-import CreateArtist from './components/addingArtistByAdmin/CreateArtist.jsx';
-import CreateAlbum from './components/addingArtistByAdmin/CreateAlbum.jsx';
-import CreateTrack from './components/addingArtistByAdmin/CreateTrack.jsx';
-import ArtistPage from '../pages/ArtistPage.jsx'
-import AlbumPage from "../pages/AlbumPage.jsx";
+//import Signup from './components/Signup.jsx';
+import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
+import Artists from './components/Artists.jsx';
+import Albums from './components/Albums.jsx';
+import ArtistPage from './components/pages/ArtistPage.jsx';
+import AlbumPage from './components/pages/AlbumPage.jsx';
+import Login from './components/Login.jsx';
+import CreateArtist from './components/content_managment_system/CreateArtist.jsx';
+import CreateAlbum from './components/content_managment_system/CreateAlbum.jsx';
+import CreateTrack from './components/content_managment_system/CreateTrack.jsx';
+
+
 
 function App() {
     return (
@@ -20,9 +22,9 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/artists" element={<Artists />} />
-                    <Route path="/artists/:id" element={<ArtistPage/>}/>
+                    <Route path="/artists/:id" element={<ArtistPage />} />
                     <Route path="/albums" element={<Albums />} />
-                    <Route path="/albums/:id" element={<AlbumPage/>}/>
+                    <Route path="/albums/:id" element={<AlbumPage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/createartist" element={<CreateArtist />} />
                     <Route path="/createalbum" element={<CreateAlbum />} />
@@ -31,7 +33,6 @@ function App() {
             </div>
         </HashRouter>
     );
-
 }
 
 export default App;
--- client/src/components/FeaturedArtists.jsx
+++ client/src/components/FeaturedArtists.jsx
@@ -1,25 +1,10 @@
 import { useContext } from 'react';
 import { MyContext } from '../context/context';
 import { Link } from 'react-router-dom';
-import AlbumImage from '../../page_components/AlbumImage.jsx';
+import Artists from './Artists.jsx';
 
 const FeaturedArtists = () => {
-    const { artists } = useContext(MyContext);
-    return (
-        <div className="artists">
-            {artists.map((artist) => {
-                return (
-                    <Link to={`/artists/${artist._id}`} state={artist}>
-                        <div key={artist._id} className="small-artists">
-                            <h3>{artist.artistName}</h3>
-                            <img src={artist.artistImage} alt="artistimage" />
-                            <p>{artist.city}</p>
-                        </div>
-                    </Link>
-                );
-            })}
-        </div>
-    );
+    return <Artists />;
 };
 
 export default FeaturedArtists;
