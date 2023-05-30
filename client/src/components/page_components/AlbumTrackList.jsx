c3149a03072d314075545df0eb4eda3e49a64c79
maurofilippifarmar <mauro.filippifarmar@gmail.com>
2023-05-30 11:17:59 +0200

    started with the player

--- client/src/components/page_components/AlbumTrackList.jsx
+++ client/src/components/page_components/AlbumTrackList.jsx
@@ -1,13 +1,28 @@
+import { Howl, Howler } from 'howler';
+
 const AlbumTrackList = ({ tracklist }) => {
-    console.log('tracklist', tracklist)
     return (
         <div>
             <ul>
                 {tracklist?.map((track) => {
+                    console.log('tracklist', track.trackFile);
+                   
+                    const sourceAudio = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3';
+                   //const sourceAudio = track.tarckFile;
+                        const sound = new Howl({
+                            src: [sourceAudio],
+                            html5: true,
+                            format: ['mp3'],
+                        });
+                        sound.play();
+                    
+
                     return (
                         <li key={track?._id}>
                             <span>{track?.trackName}</span>
-                            <span>{track?.duration}</span>
+                            <div onClick={() => sound.play()}>PLAY</div>
+                            <div onClick={()=> sound.pause()}>PAUSE</div>
+                            <div onClick={() => sound.stop()}>STOP</div>
                         </li>
                     );
                 })}
